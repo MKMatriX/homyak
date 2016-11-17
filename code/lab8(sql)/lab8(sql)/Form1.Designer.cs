9b83e138951df675830bfbfd06ec7de982a14cc1
MKMatriX <matrix-elf@yandex.ru>
2016-11-18 00:18:20 +0700

    initial commit

---
+++ code/lab8(sql)/lab8(sql)/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace lab8_sql_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(460, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(183, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Школа";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 529);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ code/lab8(sql)/lab8(sql)/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// Как люди вообще могут сидеть в этой IDE даже vi удобней чем она, а ему уже лет 30.
+
+namespace lab8_sql_
+{
+    
+
+    public struct Student : IComparable
+    {
+        public string name;
+        public string lastName;
+        public string thirdName;
+        public int birthYear;
+        public string adress;
+        public string school;
+
+        public int CompareTo(object obj)
+        {
+            Student rival = (Student)obj;
+            return this.birthYear - rival.birthYear;
+        }
+
+        public override string ToString() // for human readable output
+        {
+            string res = "";
+            res += "Имя: " + this.name+"\n";
+            res += "Фамилия: " + this.lastName + "\n";
+            res += "Отчество: " + this.thirdName + "\n";
+            res += "Год рождения: " + this.birthYear.ToString() + "\n";
+            res += "Адресс: " + this.adress + "\n";
+            res += "Школа: " + this.school + "\n";
+            return res;
+        }
+
+        public string ToCsvString() // we will use that to save oustput same as input
+        {
+            string res = "";
+            res += this.name + ";";
+            res += this.lastName + ";";
+            res += this.thirdName + ";";
+            res += this.birthYear.ToString() + ";";
+            res += this.adress + ";";
+            res += this.school + "\r\n";
+            return res;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private Student[] students = new Student[1000];
+        private string inputContent;
+        private const int rowLength = 6;
+
+        // function to add an item to our table
+        public Student lineToStudent(string line)
+        {
+            string[] cells = line.Split(";".ToCharArray());
+            if (cells.Length != 6) {
+                // if row isn't valid then we will throw an  exption
+                throw new Exception("invalid number of parameters");
+            }
+            Student row = new Student(); // how I missed jss 'call'
+            row.name = cells[0].Trim();
+            row.lastName = cells[1].Trim();
+            row.thirdName = cells[2].Trim();
+            row.birthYear = Convert.ToInt32(cells[3].Trim(), 10); // this too will throw an error that we should catch
+            row.adress = cells[4].Trim();
+            row.school = cells[5].Trim();
+
+            // add some sense here, for example for a birthYear
+            if (row.birthYear < 1900 || row.birthYear > 2020)
+            {
+                throw new Exception("suspicious birth year: "+ row.birthYear);
+            }
+            return row;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Saving...";
+            this.saveFile();
+            
+        }
+
+        private void eventLog1_EntryWritten(object sender, System.Diagnostics.EntryWrittenEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Press left top button to start ^_^";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void saveFile()
+        {
+            string output = "";
+            string selectedSchool = (comboBox1.SelectedItem as ComboboxItem).Text;
+            Student[] selectedSchoolStudents = this.students.Where(student => student.school == selectedSchool).ToArray(); // some more functional programming
+            for (int i = 0; i < selectedSchoolStudents.Length; i++)
+            {
+                output += selectedSchoolStudents[i].ToCsvString();
+            }
+
+            using (StreamWriter file = new StreamWriter("output.txt"))
+            {
+                file.WriteLine(output);
+            }
+            label1.Text = "Save complete, found " + selectedSchoolStudents.Length.ToString() + " stunedt(s)";
+        }
+
+        public void addSchoolsToList()
+        {
+            string[] schools = this.students.Select(student => student.school).Distinct().ToArray(); // some part of functional programming
+
+            for (int i = 0; i < schools.Length; i++)
+            {
+                ComboboxItem item = new ComboboxItem();
+                item.Text = schools[i];
+                item.Value = schools[i]; // who the hell cares?
+                comboBox1.Items.Add(item);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        public void loadFile() {
+            label1.Text = "Loading file...";
+            try
+            {
+                using (StreamReader sr = new StreamReader("input.txt"))
+                {
+                    String inputFile = sr.ReadToEnd();
+                    this.inputContent = inputFile;
+                    // label1.Text = inputFile;
+                    label1.Text = "Parsing file...";
+                    string[] lines = inputFile.Split("\n".ToCharArray());
+
+                    List<Student> tmpList = new List<Student>(); // to avoid hardcoded length of the students array
+                    label1.Text = "";
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        try
+                        {
+                            // it's our function, and it still can throw an error
+                            Student row = this.lineToStudent(lines[i]);
+                            tmpList.Add(row);
+                            label1.Text += "Student " + "(" + i + ") added successfuly:\n" + row.ToString() + "\n";
+                        }
+                        catch (Exception er)
+                        {
+                            // so we'll handle its log
+                            string err = "Line(" + i + ") parse error: \n >>> " + er.Message + "\n";
+                            label1.Text += err;
+                        }
+
+                    }
+                    this.students = tmpList.ToArray();
+                    Array.Sort(this.students);
+                    this.addSchoolsToList();
+                }
+            }
+            catch (Exception er)
+            {
+                string err = "Catched error, the file could not be read: \n" + er.Message;
+                label1.Text = err;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.loadFile();
+        }
+    }
+
+    public class ComboboxItem // one of ways to add items to combobox
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
